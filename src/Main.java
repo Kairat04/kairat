cd9f6aec39d28a69ff55ed239b1e2d75e8c54bce
kairat_tunkatarov <kairat05062004@gmail.com>
2022-11-13 23:38:19 +0600

    commit for local repo

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("I commit new commit");
+        System.out.println("It line only for location repositories");
     }
 }
